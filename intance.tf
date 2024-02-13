79a7113de5c1c9643bbfca4c2e5baa8d94ce890b
abhi6093 <abhishekdal05@mail.com>
2024-02-13 19:22:10 +0530

    smalll.update

--- intance.tf
+++ intance.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "my_instance" {
     ami = var.image_id
     instance_type = var.machine_type
     key_name = "terraform_key"
-    vpc_security_group_ids = ["sg-011d824db10ce7685"]
+    vpc_security_group_ids = ["sg-011d824db10ce7685", aws_security_group.my_sg_group.id]
     tags = {
         name = "my-instance"
         env = "dev"
