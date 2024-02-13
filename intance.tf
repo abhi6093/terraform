2e8799a67d413e9a7a69bee4a1495395751b3b14
abhi6093 <abhishekdal05@mail.com>
2024-02-13 17:32:48 +0530

    new-changes

--- intance.tf
+++ intance.tf
@@ -3,7 +3,7 @@ region = "ap-northeast-3"
 }
 resource "aws_instance" "my_instance" {
     ami = "ami-05ff0b3a7128cd6f8"
-    insatnce_type = "t2.micro"
+    instance_type = "t2.micro"
     key_name = "docker-key"
     vpc_security_group_ids = ["sg-011d824db10ce7685"]
 }
