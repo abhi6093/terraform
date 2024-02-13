034e7d489c112af4ce448064da015686bd150a02
abhi6093 <abhishekdal05@mail.com>
2024-02-13 18:03:04 +0530

    edits_made

--- intance.tf
+++ intance.tf
@@ -2,8 +2,16 @@ provider "aws" {
 region = "ap-northeast-3"
 }
 resource "aws_instance" "my_instance" {
-    ami = "ami-05ff0b3a7128cd6f8"
-    instance_type = "t2.micro"
+    ami = var.image_id
+    instance_type = var.machine_type
     key_name = "terraform_key"
     vpc_security_group_ids = ["sg-011d824db10ce7685"]
+}
+
+variable "image_id" {
+    default = "ami-05ff0b3a7128cd6f8"
+}
+
+variable "machine_type" {
+    default = "t2.micro"
 }
