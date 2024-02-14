2168b69bda8f30983d7d9792c04c9e9f6649d193
abhi6093 <abhishekdal05@mail.com>
2024-02-14 23:14:49 +0530

    ioiuuu

--- infrastructure/homework-IAM-user/user.tf
+++ infrastructure/homework-IAM-user/user.tf
@@ -1,19 +1,19 @@
 provider "aws" {
     region = "ap-northeast-3"
 }
-resource "aws_iam_user" "tf" {
+resource "aws_iam_user" "lb" {
     name = "terraform_user"
     tags = {
     tag-key = "tag-value" 
    }
 }
 
-resource "aws_iam_access_key"  "tf" {
-    user = "aws_iam_user.tf.name"
+resource "aws_iam_access_key"  "lb" {
+    user = "aws_iam_user.lb.name"
 }
 
 resource "aws_s3_bucket" "practice" {
-    bucket = "my-tf-practice-bucket"
+    bucket = "my-lb-practice-bucket"
     tags = {
         Name = "My-bucket-practice"
         Environment = "Dev"
