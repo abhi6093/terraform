33f938af0b0ec1f42deede45ca29f6b31b68cb28
abhi6093 <abhishekdal05@mail.com>
2024-02-14 23:30:22 +0530

    updated-terraform

--- infrastructure/homework-IAM-user/user.tf
+++ infrastructure/homework-IAM-user/user.tf
@@ -1,21 +1,50 @@
 provider "aws" {
     region = "ap-northeast-3"
 }
-resource "aws_iam_user" "lb" {
-    name = "terraform_user"
-    tags = {
-    tag-key = "tag-value" 
-   }
+
+resource "aws_iam_user" "nu" {
+  name = "new-user"
+  tags = {
+    tag-key = "tag-value"
+  }
 }
 
-resource "aws_iam_access_key"  "lb" {
-    user = "aws_iam_user.lb.name"
+resource "aws_s3_bucket" "practice_bucket" {
+  bucket = "var.s3-bucket-name"
+  tags = {
+    Name        = "new-bucket"
+    Environment = "Dev"
+  }
+}  
+
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "s3_access_policy"
+  description = "IAM policy for S3 bucket access"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket",
+        ],
+        Resource = [
+          aws_s3_bucket.practice_bucket.arn,
+          "${aws_s3_bucket.practice_bucket.arn}/*",
+        ],
+      },
+    ],
+  })
 }
 
-resource "aws_s3_bucket" "practice" {
-    bucket = "my-lb-practice-bucket"
-    tags = {
-        Name = "My-bucket-practice"
-        Environment = "Dev"
-    }
+resource "aws_iam_user_policy_attachment" "s3_access_attachment" {
+  user       = aws_iam_user.nu.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+variable "s3-bucket-name" {
+  default = "bucket-from-terraform"
 }
