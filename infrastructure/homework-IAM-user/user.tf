9501d445cac4bee7ffee34192b7e1725fea31587
abhi6093 <abhishekdal05@mail.com>
2024-02-15 00:21:05 +0530

    final-update

--- infrastructure/homework-IAM-user/user.tf
+++ infrastructure/homework-IAM-user/user.tf
@@ -1,54 +1,37 @@
 provider "aws" {
-    region = "ap-south-1"
+    region = "ap-northeast-3"
 }
-
-resource "aws_iam_user" "lb" {
-  name = "loadbalancer"
-  path = "/system/"
-
-  tags = {
-    tag-key = "tag-value"
-  }
+resource "aws_iam_user" "new_user" {
+    name = "new-user"
+    tags = {
+        Name = "New-User"
+    }
 }
-
-resource "aws_s3_bucket" "A" {
-  bucket = "var.s3-bucket-name"
+resource "aws_s3_bucket" "new_bucket" {
+  bucket = "my-tf-test-bucket"
   tags = {
-    Name        = "My bucket"
+    Name        = "New-Bucket"
     Environment = "Dev"
   }
-
-}  
-
-
+}
 resource "aws_iam_policy" "s3_access_policy" {
   name        = "s3_access_policy"
   description = "IAM policy for S3 bucket access"
-
+ 
+ 
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
         Effect   = "Allow",
-        Action   = [
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket",
-        ],
-        Resource = [
-          aws_s3_bucket.A.arn,
-          "${aws_s3_bucket.A.arn}/*",
-        ],
+        Action   = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
+        Resource = [aws_s3_bucket.new_bucket.arn, "${aws_s3_bucket.new_bucket.arn}/*"],
       },
     ],
   })
 }
 
 resource "aws_iam_user_policy_attachment" "s3_access_attachment" {
-  user       = aws_iam_user.lb.name
+  user       = aws_iam_user.new_user.name
   policy_arn = aws_iam_policy.s3_access_policy.arn
-}
- variable "s3-bucket-name" {
-  type    = string
-  default = "my-tf-test-bucket0707"
 }
