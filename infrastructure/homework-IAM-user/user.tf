50351a60ec7f7e3bc91b56884be10ce608df3a50
abhi6093 <abhishekdal05@mail.com>
2024-02-15 11:29:03 +0530

    final-update

--- infrastructure/homework-IAM-user/user.tf
+++ infrastructure/homework-IAM-user/user.tf
@@ -1,37 +1,51 @@
 provider "aws" {
     region = "ap-northeast-3"
 }
-resource "aws_iam_user" "new_user" {
-    name = "new-user"
-    tags = {
-        Name = "New-User"
-    }
-}
-resource "aws_s3_bucket" "new_bucket" {
-  bucket = "terraform-bucket"
+
+resource "aws_iam_user" "nu" {
+  name = "new-user"
   tags = {
-    Name        = "New-Bucket"
-    Environment = "Dev"
+    tag-key = "new-user"
   }
 }
+
+resource "aws_s3_bucket" "CB" {
+  bucket = "var.s3-bucket-name"
+  acl = public
+  tags = {
+    Name        = "new-bucket"
+    Environment = "Dev"
+  }
+}  
+
 resource "aws_iam_policy" "s3_access_policy" {
   name        = "s3_access_policy"
   description = "IAM policy for S3 bucket access"
- 
- 
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
         Effect   = "Allow",
-        Action   = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"],
-        Resource = [aws_s3_bucket.new_bucket.arn, "${aws_s3_bucket.new_bucket.arn}/*"],
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket",
+        ],
+        Resource = [
+          aws_s3_bucket.CB.arn,
+          "${aws_s3_bucket.CB.arn}/*",
+        ],
       },
     ],
   })
 }
 
 resource "aws_iam_user_policy_attachment" "s3_access_attachment" {
-  user       = aws_iam_user.new_user.name
+  user       = aws_iam_user.nu.name
   policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+variable "s3-bucket-name" {
+  default = "bucket-from-terraform"
 }
