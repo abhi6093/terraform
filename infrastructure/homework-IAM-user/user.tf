2ccc9e672da5cbdc232f3948c212ea16507c1194
abhi6093 <abhishekdal05@mail.com>
2024-02-15 11:53:15 +0530

    ll

--- infrastructure/homework-IAM-user/user.tf
+++ infrastructure/homework-IAM-user/user.tf
@@ -44,7 +44,3 @@ resource "aws_iam_user_policy_attachment" "s3_access_attachment" {
   user       = aws_iam_user.nu.name
   policy_arn = aws_iam_policy.s3_access_policy.arn
 }
-
-*/variable "s3-bucket-name" {
-  default = "bucket-from-terraform"
-}*/
