c9d985994682e227e4f972ce93d2d4ed057a8793
abhi6093 <abhishekdal05@mail.com>
2024-02-25 19:10:23 +0530

    vpc-peering

---
+++ vpc-peering.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+    region "ap-northeast-3
+    }
+    
+"resource "aws_vpc" "vpc1" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "vpc2" {
+  cidr_block = "10.1.0.0/16"
+}
+
+resource "aws_vpc_peering_connection" "vpcpeering" {
+  vpc_id      = aws_vpc.vpc1.id
+  peer_vpc_id = aws_vpc.vpc2.id
+  auto_accept = true
+}
+
+resource "aws_vpc_peering_connection_options" "appectingvpc" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpcpeering.id
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
